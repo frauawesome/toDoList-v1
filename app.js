694a25436784c9b75986cfd8f9e0cd661c9dec2d
Carly Schmitting <cschmitting@gmail.com>
2020-08-16 23:23:46 +0200

    add views and css for list and update app.js accordingly

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+
+var items = [];
 
 app.get("", function(req, res){
-    
     var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if (today.getDay() == 6 || today.getDay() == 0) {
-        res.sendFile(__dirname + "/index.html");
-    } else {
-        res.send("Boo! I have to work!");
-    }    
+    var day = today.toLocaleDateString("en-US", options);
+ 
+    res.render("list", {
+       day: day, 
+       newListItems: items 
+    });
 
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
